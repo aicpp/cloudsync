5c7b8c760cc66f1e5263b5e9c63e4d253a3c2305
Kyle Barry <kyle@quantsolutions.co.za>
2022-03-01 09:37:03 +0200

    update dropbox and add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cloudsync',
+    version='0.1',
+    description='A useful module',
+    author='aicpp(https://github.com/aicpp), Kyle Barry',
+    packages=['cloudsync'],
+    install_requires=['dropbox'],
+)
